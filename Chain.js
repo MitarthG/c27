336e301cf1a5dec7c8342f3f4e674b8e4c2e8d07
MitarthG <77493134+MitarthG@users.noreply.github.com>
2021-04-27 15:13:19 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,22 @@
+class Chain
+{
+  constructor(bodyA,bodyB)
+  {
+      var options={
+          bodyA:bodyA.body,
+          bodyB:bodyB.body,
+          stiffness:0.05,
+          length:10
+      }
+      this.chain=Constraint.create(options);
+      World.add(world,this.chain);
+  }
+
+display(){
+    var a=this.chain.bodyA.position;
+    var b=this.chain.bodyB.position;
+
+    strokeWeight(4);
+    line (a.x,a.y,b.x,b.y);
+}
+}
